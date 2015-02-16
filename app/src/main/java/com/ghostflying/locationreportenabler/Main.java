31332175ab999892448c385622d96019b966dda4
GhostFlying <GhostFlying@Gmail.com>
2015-02-16 10:24:37 +0800

    clean project for xposed

---
+++ app/src/main/java/com/ghostflying/locationreportenabler/Main.java
@@ -0,0 +1,14 @@
+package com.ghostflying.locationreportenabler;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by ghostflying on 2/16/15.
+ */
+public class Main implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+    }
+}
