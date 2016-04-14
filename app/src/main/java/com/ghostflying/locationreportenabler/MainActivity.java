f062a7db352cabd708cd4dc4c4802d0b030e0f13
ghostflying <GhostFlying@Gmail.com>
2016-04-14 20:18:26 +0800

    add more function for convenience

--- app/src/main/java/com/ghostflying/locationreportenabler/MainActivity.java
+++ app/src/main/java/com/ghostflying/locationreportenabler/MainActivity.java
@@ -5,25 +5,44 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    private boolean[] function_enable = new boolean[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(getString(R.string.dialog_content))
-                .setPositiveButton(getString(R.string.dialog_button), new DialogInterface.OnClickListener() {
+        Toast.makeText(this, R.string.prompt_toast, Toast.LENGTH_LONG).show();
+        getFunctionDialogBuilder().create().show();
+    }
+
+    private AlertDialog.Builder getFunctionDialogBuilder(){
+        boolean[] selected = new boolean[]{
+            true, false, false, false
+        };
+        function_enable[0] = true;
+
+        return new AlertDialog.Builder(this)
+                .setTitle(R.string.function_choice_dialog_title)
+                .setMultiChoiceItems(R.array.dialog_choices, selected, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        function_enable[which] = isChecked;
+                    }
+                })
+                .setPositiveButton(R.string.function_choice_dialog_button, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        PropUtil.enableLocationReport();
-                        hideLauncher();
+                        if (function_enable[0]){
+                            hideLauncher();
+                            PropUtil.applyFunctions(function_enable);
+                        }
                         finish();
                     }
                 });
-        builder.create().show();
     }
 
     private void hideLauncher(){
--- app/src/main/java/com/ghostflying/locationreportenabler/PropUtil.java
+++ app/src/main/java/com/ghostflying/locationreportenabler/PropUtil.java
@@ -8,7 +8,14 @@ import java.io.IOException;
  */
 public final class PropUtil {
     private static final String COMMAND_PREFIX = "setprop ";
-    private static final String[] PROPERTIES = {"gsm.sim.operator.numeric 310030"};
+    private static final String[] PROPERTIES = {
+            "gsm.sim.operator.numeric 310030",
+            "gsm.sim.operator.iso-country us"
+    };
+    private static final String COMMAND_CLEAR_PREFIX = "pm clear ";
+    private static final String PKG_GMS = "com.google.android.gms";
+    private static final String PKG_MAPS = "com.google.android.apps.maps";
+    private static final String COMMAND_REBOOT = "reboot";
 
     public static void enableLocationReport() {
         try{
@@ -17,6 +24,37 @@ public final class PropUtil {
             for (String property : PROPERTIES) {
                 os.writeBytes(COMMAND_PREFIX + property + "\n");
             }
+            os.writeBytes("exit\n");
+            os.flush();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void applyFunctions(boolean[] params){
+        try{
+            Process p = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(p.getOutputStream());
+
+            if (params[0]){
+                for (String property : PROPERTIES) {
+                    os.writeBytes(COMMAND_PREFIX + property + "\n");
+                }
+            }
+
+            if (params[1]){
+                os.writeBytes(COMMAND_CLEAR_PREFIX + PKG_GMS + "\n");
+            }
+
+            if (params[2]){
+                os.writeBytes(COMMAND_CLEAR_PREFIX + PKG_MAPS + "\n");
+            }
+
+            if (params[3]){
+                os.writeBytes(COMMAND_REBOOT + "\n");
+            }
+
             os.writeBytes("exit\n");
             os.flush();
         }
